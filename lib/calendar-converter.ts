864bb495c9f8eabc95236a2c9f4f5b322e793509
srisur96 <srilaksana1923@gmail.com>
2025-07-03 02:23:17 +0100

    Update calendar-converter.ts

--- lib/calendar-converter.ts
+++ lib/calendar-converter.ts
@@ -112,7 +112,7 @@ export function convertGregorianToJavanese(date: Date) {
   const deltaDaysPawukon = Math.floor((dateUTC.getTime() - refPawukonDate.getTime()) / oneDay);
 
   const weekdayIndex = (dateUTC.getUTCDay() + 6) % 7; // Senin = 0
-  const pasaranIndex = (deltaDaysPawukon + refPasaranIndex + 4) % 5;
+  cconst pasaranIndex = (refPasaranIndex + (deltaDaysPawukon % 5) + 5) % 5;
   const wukuIndex = (refWukuIndex + Math.floor((((deltaDaysPawukon + 1) % 210 + 210) % 210) / 7)) % 30;
 
   return {
